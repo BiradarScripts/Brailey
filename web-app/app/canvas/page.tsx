4948e3c696002c7193e771ee0b4e7135a8384419
Shreyas <Shreyas.Biradar@iiitb.ac.in>
2024-11-17 23:03:37 +0530

    updated client and server

---
+++ web-app/app/canvas/page.tsx
@@ -0,0 +1,9 @@
+import BlockPage from "@/components/app-canvas-folder-id-note-id-page";
+
+export default function Page() {
+  return (
+    <>
+      <BlockPage/>
+    </>
+  );
+}
---
+++ web-app/app/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import BlockPage from "@/components/app-dashboard-page";
+
+export default function Page() {
+  return (
+    <>
+      <BlockPage/>
+    </>
+  );
+}
